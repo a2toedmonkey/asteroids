4c2de3c892806e8ffadc041c91d55e088e6b7bbf
a2toedmonkey <a2toedmonkey@gmail.com>
2025-02-23 02:00:28 -0600

    Setup basics and import pygame, as well as create loop

--- main.py
+++ main.py
@@ -2,9 +2,28 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-    
+from constants import *
+   
 def main():
-    print("Starting asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    while True:
+        screen.fill((0,0,0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+       
+       
+       
+       
+       
+        pygame.display.flip()
+    
 
 if __name__ == "__main__":
     main()
+
